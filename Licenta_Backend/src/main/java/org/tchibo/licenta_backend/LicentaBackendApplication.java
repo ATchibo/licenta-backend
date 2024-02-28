eed2d5bc077c07b65ba0fbd38eb2d9105404cf00
Andrei Cibotariu <andreicibotariu@outlook.com>
2024-02-28 16:57:05 +0200

    added dependencies

---
+++ Licenta_Backend/src/main/java/org/tchibo/licenta_backend/LicentaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.tchibo.licenta_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicentaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LicentaBackendApplication.class, args);
+    }
+
+}
---
+++ Licenta_Backend/src/test/java/org/tchibo/licenta_backend/LicentaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tchibo.licenta_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicentaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
